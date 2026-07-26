b63ac3a6fec95595d70b5d68b1264bc71422fa99
lindyhsu0217 <smilleywen0217@gmail.com>
2026-07-26 12:31:32 +0100

    Add promotion profitability SQL analysis

---
+++ sql/01_promotion_profitability.sql
@@ -0,0 +1,14 @@
+/*
+Business Question
+
+Which promotions generated the highest profit?
+
+Objective
+
+Compare promotion performance and identify which promotions increased profit instead of only increasing sales.
+
+Status
+
+Work in progress.
+
+*/
